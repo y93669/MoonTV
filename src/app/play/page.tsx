7fb81624d2ab9586f713adf073812dd311da571f
y93669 <114243660+y93669@users.noreply.github.com>
2025-07-20 10:48:48 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1048,7 +1048,8 @@ function PlayPageClient() {
 
     try {
       // 创建新的播放器实例
-      Artplayer.PLAYBACK_RATE = [0.5, 0.75, 1, 1.25, 1.5, 2];
+      Artplayer.FAST_FORWARD_VALUE = 2;
+      Artplayer.PLAYBACK_RATE = [0.5, 0.75, 1, 1.25, 1.5, 2, 3];
       Artplayer.USE_RAF = true;
 
       artPlayerRef.current = new Artplayer({
